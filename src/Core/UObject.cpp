e748eb8141d0f3bea41653c185ab389c251be212
Adam Smith <axs3960@rit.edu>
2019-01-16 13:48:17 -0500

    Level object documentation mostly complete

--- src/Core/UObject.cpp
+++ src/Core/UObject.cpp
@@ -285,7 +285,7 @@ void UObject::ReadDefaultProperties()
       {
         if ( Outer )
           Logf( LOG_CRIT, "Property '%s' in '%s.%s.%s' does not exist",
-            PropName, Pkg->Name, Outer->Name, Name );
+            PropName, Outer->Pkg->Name, Outer->Name, Name );
         else
           Logf( LOG_CRIT, "Property '%s' in '%s.%s' does not exist",
             PropName, Pkg->Name, Name );
