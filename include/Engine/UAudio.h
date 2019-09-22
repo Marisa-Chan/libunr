865392ee3c9bdd6cc41231ef2c5deeb5d0b3b72c
Adam Smith <remodeat@gmx.com>
2019-09-22 11:19:34 -0400

    Change license from AGPLv3 to GPLv3, native server side protection might be nice to have closed source

--- include/Engine/UAudio.h
+++ include/Engine/UAudio.h
@@ -3,17 +3,17 @@
 |*  Copyright (C) 2018-2019  Adam W.E. Smith                                 *|
 |*                                                                           *|
 |*  This program is free software: you can redistribute it and/or modify     *|
-|*  it under the terms of the GNU Affero General Public License as           *|
-|*  published by the Free Software Foundation, either version 3 of the       *|
-|*  License, or (at your option) any later version.                          *|
+|*  it under the terms of the GNU General Public License as published by     *|
+|*  the Free Software Foundation, either version 3 of the License, or        *|
+|*  (at your option) any later version.                                      *|
 |*                                                                           *|
 |*  This program is distributed in the hope that it will be useful,          *|
 |*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *|
 |*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *|
-|*  GNU Affero General Public License for more details.                      *|
+|*  GNU General Public License for more details.                             *|
 |*                                                                           *|
-|*  You should have received a copy of the GNU Affero General Public License *|
-|*  along with this program.  If not, see <https://www.gnu.org/licenses/>.   *|
+|*  You should have received a copy of the GNU General Public License        *|
+|*  along with this program. If not, see <https://www.gnu.org/licenses/>.    *|
 \*===========================================================================*/
 
 /*========================================================================
@@ -33,6 +33,18 @@ class DLL_EXPORT UAudioSubsystem : public USubsystem
 
   UAudioSubsystem();
 
-  // TODO:
+  virtual void SetViewport( UViewport* Viewport ) {}
+  virtual void RegisterSound( USound* Sound ) {}
+  virtual void RegisterMusic( UMusic* Music ) {}
+  virtual void UnregisterSound( USound* Sound ) {}
+  virtual void UnregisterMusic( UMusic* Music ) {}
+  virtual bool PlaySound( AActor* Actor, USound* Sound, FVector Location, float Volume, float Radius, float Pitch ) 
+  {
+    return false;
+  }
+  virtual bool DoMusicTransition( UMusic* Music, EMusicTransition MusicTrans ) 
+  {
+    return false;
+  }
 };
 
